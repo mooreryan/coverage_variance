62818cdc014dec51129bec27ec5f6ac543a2fdae
Ryan Moore <moorer@udel.edu>
2015-10-12 12:40:21 -0400

    Take any size genome

--- coverage_variance.c
+++ coverage_variance.c
@@ -1,32 +1,33 @@
 /*
-Copyright 2015 Ryan Moore
-Contact: moorer@udel.edu
+  Copyright 2015 Ryan Moore
+  Contact: moorer@udel.edu
 
-coverage_variance is free software: you can redistribute it and/or
-modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+  coverage_variance is free software: you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
 
-coverage_variance is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  coverage_variance is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with coverage_variance.  If not, see
-<http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with coverage_variance.  If not, see
+  <http://www.gnu.org/licenses/>.
 */
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
   int lineno = 0;
   int idx = 0;
-  int size = 10000000; // ten million max length of contig
+  int size = 100000; // 100,000 starting contig size
   int prev_pos, pos, cov, same_contig;
   double total_cov = 0;
   double mean = 0;
@@ -35,12 +36,11 @@ int main(int argc, char* argv[])
   double sd = 0;
   double cv = 0;
 
-  /* char prev_contig[100], contig[100]; */
+  /* will handle an absurd 1000 char contig name */
+  char *prev_contig = calloc(1000, sizeof(char));
+  char *contig      = calloc(1000, sizeof(char));
 
-  char* prev_contig = calloc(1000, sizeof(char));
-  char* contig      = calloc(1000, sizeof(char));
-
-  int* coverages    = calloc(size, sizeof(int));
+  int *coverages    = calloc(size, sizeof(int));
 
   printf("contig\t"
          "contig.length\t"
@@ -51,6 +51,18 @@ int main(int argc, char* argv[])
 
   while(scanf("%s\t%d\t%d", contig, &pos, &cov) == 3) {
     lineno += 1;
+
+    if (pos > size) {
+      /* fprintf(stderr, "DOUBLING SIZE\n"); */
+      size *= 2;
+      assert(size > 0);
+
+      int *tmp = realloc(coverages, size * sizeof(int));
+      assert(tmp != NULL);
+
+      coverages = tmp;
+      assert(coverages != NULL);
+    }
 
     same_contig = strcmp(prev_contig, contig);
 
